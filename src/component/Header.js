61776bf482754c5ab664a4f83120995fe91b7682
Thithran <thi.tran.fi@gmail.com>
2019-03-04 08:57:11 +0200

    changing page layout

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
 const Header = () => (
-    <div> 
-        <h1>No Loan Pay</h1>
-        <h3>You will never pay shitty money anymore!</h3>
-
+    <div className="App"> 
+        <div className="header">
+            <h1>No Loan Pay</h1>
+            <h3>You will never pay shitty money anymore!</h3>
+        </div>
         <NavLink to="/" activeClassName="is-active" exact={true}><button type="button" className="btn btn-primary mx-1">My Current Loan</button></NavLink> 
         <NavLink to="/add" ><button type="button" className="btn btn-info mx-1">Add New Loan</button></NavLink> 
     </div>
